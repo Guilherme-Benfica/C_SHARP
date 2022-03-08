b061d2f910470422d05fb0cd02b9dd7e51c8688d
Guilherme Benfica <99348004+Guilherme-Benfica@users.noreply.github.com>
2022-03-08 20:18:33 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Programa {
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            int hora;
+
+            Console.Write("Digite uma hora do dia: ");
+            hora = int.Parse(Console.ReadLine());
+           
+            if (hora < 12) {
+                Console.WriteLine("Bom dia!");
+            }
+           if else {
+                Console.WriteLine("Boa tarde!");
+
+          
+                
+            }
+        }
+    }
+}
