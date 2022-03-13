a6cd93cc322fde11527f19a867538e99e330c61f
Guilherme Benfica <99348004+Guilherme-Benfica@users.noreply.github.com>
2022-03-13 14:18:18 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using System.Globalization;
+
+namespace Programa
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos numeros inteiros voce vai digitar? ");
+            int N = int.Parse(Console.ReadLine());
+
+            int soma = 0;
+            for (int i = 1; i <= N; i++)
+            {
+                Console.WriteLine("valor #{0}: ", i);
+                int valor = int.Parse(Console.ReadLine());
+                soma += valor;
+
+            }
+
+            Console.WriteLine("Soma = " + soma);
+        }
+    }
+} 
+  
